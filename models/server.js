15e4f8c323926e2c097a013734cd230c04bca600
Sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-02-13 14:48:03 -0600

    Se agrego el JS del servidor

--- models/server.js
+++ models/server.js
@@ -1,2 +1,37 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { dbConnection } = require('../db/config');
+
+class Server{
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.studentPath = '/api/estudiantes';
+
+        this.conectarDB();
+        this.middlewares();
+        this.routers();
+    }
+
+    async conectarDB() {
+        await dbConnection();
+    }
+
+    middlewares() {
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes() {
+        this.app.use(this.studentPath,require('../routes/student.routes'))
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+           console.log('Servidor ejecutandose y escuchando al puerto', this.port) 
+        });
+    }
+}
+
+module.exports = Server;
