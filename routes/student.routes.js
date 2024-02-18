498933d8c900f9e7019c9bf9b6dc40b968750673
sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-02-18 17:15:40 -0600

    Se agrego el router del rol estudiantes

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -2,9 +2,46 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
+
+const {
+    studentGet,
+    studentsPost,
+    studentsGetById,
+    putStudents
+} =require('../controllers/student.controller');
+
 const { studentGet } = require('../controllers/student.controller');
+const { existeEstudianteById } = require('../helpers/db-validator');
 
 const router = Router();
 
 router.get("/", studentGet);
+
+router.get("/:id",
+    [
+        check('id', 'No es un id valido').isMongoId(),
+        check('id').custom(existeEstudianteById),
+        validarCampos
+    ], studentsGetById);
+
+router.post(
+    "/",
+    [
+        check("nombre", "El nombre del estudiante no puede estar vacio").not().isEmpty(),
+        check("correo", "El correo no puede estar vacio").not().isEmpty(),
+        check("password", "La contraseña no puede estar vacio").not().isEmpty(),
+        validarCampos
+    ], studentsPost
+);
+
+router.put(
+    "/:id",
+    [
+        check('id', 'No es un id valido').isMongoId(),
+        checkk('id').custom(existeEstudianteById),
+        validarCampos
+    ], putStudents
+);
+
+module.exports = router;
 
